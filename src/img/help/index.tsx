90d29cf5b5c53aada551a6ceee2dadb9af8afd03
Canon <22649817+cm090@users.noreply.github.com>
2024-02-15 15:11:44 -0500

    Clean up imports

--- src/img/help/index.tsx
+++ src/img/help/index.tsx
@@ -1,15 +1,7 @@
-import printersBW from "./bw.png";
-import printersColor from "./librarycolor.png";
-import printersRicoh from "./ricoh.png";
 import cancel from "./cancel";
 import connecting from "./connecting";
+import printerLists from "./lists";
 import release from "./release";
 import select from "./select";
 
-const printerLists = {
-  bw: printersBW,
-  color: printersColor,
-  ricoh: printersRicoh,
-};
-
-export { printerLists, cancel, connecting, release, select };
+export { cancel, connecting, printerLists, release, select };
---
+++ src/img/help/lists/index.tsx
@@ -0,0 +1,7 @@
+import printersBW from "./bw.png";
+import printersColor from "./librarycolor.png";
+import printersRicoh from "./ricoh.png";
+
+const lists = { bw: printersBW, color: printersColor, ricoh: printersRicoh };
+
+export default lists;
